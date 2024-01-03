9893b0b5230e38aca77ab02e7441f176d91dbdb8
birne9 <17317296268@163.com>
2024-01-03 23:52:00 +0800

    0103 配置仓库

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import './styles/reset.css'
 import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
+import pinia from './store'
 
 const app=createApp(App)
 app.use(router)
+app.use(pinia)
 app.mount("#app")
