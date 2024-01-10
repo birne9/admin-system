5f1349c91e4d771f46f251d2b2238049dda3a6fd
birne9 <17317296268@163.com>
2024-01-11 00:46:00 +0800

    wip: 配置路由

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,14 +2,28 @@ import { RouteRecordRaw, createRouter, createWebHashHistory } from "vue-router";
 import NProgress from 'nprogress'
 import 'nprogress/nprogress.css'
 // 配置路由
-const routes:Array<RouteRecordRaw>=[{
-    path:'/',
-    name:'home',
-    component: ()=>import('../pages/home/index.vue'),
+// const routes:Array<RouteRecordRaw>=[{
+//     path:'/',
+//     name:'home',
+//     component: ()=>import('@/views/home/index.vue'),
+//     meta:{},
+//     children: []
+// }]
+const modules:Record<string,any> = import.meta.glob(['./modules/*.ts'],{eager:true});
+//公用的路由 404、error
+export const aboutRouter={
+    path:'/about',
+    name:'about',
+    component: ()=>import('@/views/about/index.vue'),
     meta:{},
     children: []
-}]
-
+} as RouteRecordRaw
+//配置路由
+const routes:Array<RouteRecordRaw> = []
+Object.keys(modules).forEach((key)=>{
+    const module=modules[key].default;
+    routes.push(module)
+})
 const router=createRouter({
     history:createWebHashHistory(),
     routes
