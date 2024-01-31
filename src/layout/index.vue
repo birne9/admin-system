95ab3a442075fe8798d010e5ea211111c240f13f
zhanjiamin <17317296268@163.com>
2024-01-31 10:27:28 +0800

    fix:修改引入

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -17,7 +17,7 @@
 </template>
 <script setup lang="ts">
 import Header from "./components/Header.vue";
-import Sidebar from "./components/Sidebar.vue";
+import Sidebar from "./components/SideBar.vue";
 </script>
 <style lang="less" scoped>
 .layout {
